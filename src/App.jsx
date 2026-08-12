dc04c9b65fc79cfdaed6acb97909faa2a726d5d2
Sirwin2win <sirwin2win@gmail.com>
2026-08-12 03:17:00 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-green-700">Hello world!</p>
+    </div>
+  );
+};
+
+export default App;
